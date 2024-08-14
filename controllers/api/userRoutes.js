2f251b05a8a4f5db7b5ec06fdb8af56704c8d675
Lothy Gresser <lothygresser@Lothys-MacBook-Air.local>
2024-08-14 13:23:32 -0500

    working through errors

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 // Import the User model from the models folder
-const { User } = require('../../models');
+const { User } = require('../../models/user.Model');
 
 router.get('/login', async (req, res)=> {
   res.render('login')
--- models/user.Model.js
+++ models/user.Model.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+
 class User extends Model { }
 
 User.init(
@@ -41,38 +42,38 @@ User.init(
             allowNull: false,
         }
     },
-    {
-        hooks: {
-            beforeCreate: async (newUser) => {
-                try {
-                    newUser.password = await bcrypt.hash(newUser.password, 10);
-                    return newUser;
-                } catch (err) {
-                    console.log(err);
-                    return err;
-                }
-            },
-            beforeUpdate: async (updatedUser) => {
-                try {
-                    updatedUser.password = await bcrypt.hash(
-                        updatedUser.password,
-                        10
-                    );
-                    return updatedUser;
-                } catch (err) {
-                    console.log(err);
-                    return err;
-                }
-            }
-        },
+    // {
+    //     hooks: {
+    //         beforeCreate: async (newUser) => {
+    //             try {
+    //                 newUser.password = await bcrypt.hash(newUser.password, 10);
+    //                 return newUser;
+    //             } catch (err) {
+    //                 console.log(err);
+    //                 return err;
+    //             }
+    //         },
+    //         beforeUpdate: async (updatedUser) => {
+    //             try {
+    //                 updatedUser.password = await bcrypt.hash(
+    //                     updatedUser.password,
+    //                     10
+    //                 );
+    //                 return updatedUser;
+    //             } catch (err) {
+    //                 console.log(err);
+    //                 return err;
+    //             }
+    //         }
+    //     },
 
-    },
+    // },
     {
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'user',
+        modelName: 'user'
     }
 );
 
